21b56225697a8b642232c0a5a2bbbd5f4b77f32d
josh007 <joshuabunty28@gmail.com>
2024-11-27 23:18:15 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://joshuabunty28:bookstore@cluster0.60ee5.mongodb.net/book-store';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
